b7808e82444a6ac1f81c6942df4e5e27e06ffdd7
William Anderson <wanderson.wa252@gmail.com>
2024-06-25 22:30:27 -0400

    Cleaned up

--- src/pages/Rush/Rush.tsx
+++ src/pages/Rush/Rush.tsx
@@ -3,22 +3,19 @@ import "../Rush/Rush.css";
 
 function Rush() {
   return (
-    <>
-      <h1>Rush</h1>
-      <div className="wrapper">
-        <h2 className="rush-title">Interested in rushing KTP?</h2>
-        <div className="rush-info">
-          Follow us on instagram to stay up to date on rush!
-        </div>
-        <a
-          href="https://instagram.com/ktpnortheastern"
-          className="rush-info-item"
-        >
-          <FaInstagram className="rush-info-icon" />
-          ktpnortheastern
-        </a>
+    <div className="wrapper">
+      <h2 className="rush-title">Interested in rushing KTP?</h2>
+      <div className="rush-info">
+        Follow us on instagram to stay up to date on rush!
       </div>
-    </>
+      <a
+        href="https://instagram.com/ktpnortheastern"
+        className="rush-info-item"
+      >
+        <FaInstagram className="rush-info-icon" />
+        ktpnortheastern
+      </a>
+    </div>
   );
 }
 
