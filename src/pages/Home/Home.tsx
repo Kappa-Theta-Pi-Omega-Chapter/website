c25c3f81a897c064110c1cb1311904035de1f070
aidanniceberg <93226226+aidanniceberg@users.noreply.github.com>
2024-06-28 12:18:45 -0400

    Add Northeastern University

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import styles from "./Home.module.css";
 import { FaLocationDot } from "react-icons/fa6";
+import { IoSchool } from "react-icons/io5";
 
 function Home() {
   return (
     <>
       <h1>Kappa Theta Pi</h1>
       <h2>Professional Technology Fraternity</h2>
+      <h3><IoSchool className={styles.icon} />Northeastern University</h3>
       <h3><FaLocationDot className={styles.icon} />Boston, MA</h3>
     </>
   );
