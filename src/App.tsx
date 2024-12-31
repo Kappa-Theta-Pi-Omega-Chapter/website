1a0616537bf0b3bdffd8ce7a2056d60eee4eaf0e
martin0he <martin.hema.mh@gmail.com>
2024-12-31 14:45:03 -0500

    vite fix

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <Route path={ROUTES.RUSH} element={<Rush />} />
         <Route path={ROUTES.ABOUT_US} element={<About />} />
       </Routes>
+      <Route path="*" element={<Home />} />
     </BrowserRouter>
   );
 }
