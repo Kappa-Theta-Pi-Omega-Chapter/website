28aed20995dcd14d3056478801ab48c4e388dce4
arminapr <arminaparva@gmail.com>
2025-08-09 23:59:46 -0400

    fixing contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,22 +1,8 @@
 import { FaGithub, FaInstagram } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import styles from "./Contact.module.css";
-import ContactCard from "./components/ContactCard/ContactCard";
 
 function Contact() {
-  const contactCards = [
-    {
-      title: "Students",
-      content:
-        "Hello, prospectives! We're excited that you're interested in learning about our fraternity, and we're here to support you throughout the process. If you have any questions about our organization, upcoming events, or how to get involved, don't hesitate to contact us. Our team is ready to provide all the details you need to make an informed choice about joining us. Just send us an email at ktp.northeastern@gmail.com, and we'll respond promptly. We can't wait to connect with you and share the amazing experiences our fraternity offers.",
-    },
-    {
-      title: "Companies",
-      content:
-        "Hello, companies and potential collaborators! We appreciate your interest in partnering with us. We believe that strong partnerships are key to driving innovation and achieving success. If you're interested in exploring opportunities for collaboration, sponsorship, or other business-related inquiries, we’d love to hear from you. We're excited to discuss how we can work together to reach our shared goals and create a lasting impact. Please feel free to contact us at ktp.northeastern@gmail.com, and our dedicated team will be thrilled to connect with you. Let's embark on a journey of growth and success together.",
-    },
-  ];
-
   return (
     <div className={styles.contentWrapper}>
       <section className={styles.section}>
@@ -25,13 +11,55 @@ function Contact() {
           Reach out for questions, collaborations, or to connect with our chapter!
         </p>
       </section>
-      {/* <section className={styles.section}>
+      <section className={styles.section}>
         <div className={styles.cardGrid}>
-          {contactCards.map((c, i) => (
-            <ContactCard key={i} title={c.title} content={c.content} />
-          ))}
-        </div> 
-      </section>*/}
+          <div className={styles.contactCard}>
+            <img
+              src="/social_pic.png"
+              alt="Students Social"
+              className={styles.cardImage}
+            />
+            <div className={styles.cardContent}>
+              <h4 className={styles.cardTitle}>Students</h4>
+              <p className={styles.cardText}>
+                Hello, prospectives! We're excited that you're interested in
+                learning about our fraternity, and we're here to support you
+                throughout the process. If you have any questions about our
+                organization, upcoming events, or how to get involved, don't
+                hesitate to contact us. Our team is ready to provide all the
+                details you need to make an informed choice about joining us. Just
+                send us an email at <a href="mailto:ktp.northeastern@gmail.com" style={{ color: "#2b6cb0", textDecoration: "underline" }}>
+                  ktp.northeastern@gmail.com</a>, and we'll respond
+                promptly. We can't wait to connect with you and share the amazing
+                experiences our fraternity offers.
+              </p>
+            </div>
+          </div>
+          <div className={styles.contactCard}>
+            <div className={styles.cardContent}>
+              <h4 className={styles.cardTitle}>Companies</h4>
+              <p className={styles.cardText}>
+                Hello, companies and potential collaborators! We appreciate your
+                interest in partnering with us. We believe that strong partnerships
+                are key to driving innovation and achieving success. If you're
+                interested in exploring opportunities for collaboration,
+                sponsorship, or other business-related inquiries, we'd love to hear
+                from you. We're excited to discuss how we can work together to
+                reach our shared goals and create a lasting impact. Please feel
+                free to contact us at <a href="mailto:ktp.northeastern@gmail.com" style={{ color: "#2b6cb0", textDecoration: "underline" }}>
+                  ktp.northeastern@gmail.com</a>, and our
+                dedicated team will be thrilled to connect with you. Let's embark
+                on a journey of growth and success together.
+              </p>
+            </div>
+            <img
+              src="/professional.PNG"
+              alt="Companies Professional"
+              className={styles.cardImage}
+            />
+          </div>
+        </div>
+      </section>
       <section className={styles.section}>
         <h3 className={styles.infoHeading}>Connect with us</h3>
         <div className={styles.contactInfoGrid}>
